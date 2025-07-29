567d1e561f70835c47bbefdc3bbd6b63e1af8549
酒植 <jiuzhi@alioth.center>
2025-07-30 01:49:28 +0800

    feat(logger): add file writer

--- logger/rotator.go
+++ logger/rotator.go
@@ -7,25 +7,39 @@ import (
 	"time"
 )
 
-type TimeRotator struct {
+type noRotator struct {
+	basePath string
+}
+
+func NewNoRotator(basePath string) LogRotator {
+	return &noRotator{
+		basePath: basePath,
+	}
+}
+
+func (r *noRotator) CalculateRotationKey() (key string) {
+	return r.basePath
+}
+
+type timeRotator struct {
 	basePath   string
 	fileName   string
 	timeFormat string
 }
 
 func NewTimeRotator(basePath, fileName, timeFormat string) LogRotator {
-	return &TimeRotator{
+	return &timeRotator{
 		basePath:   basePath,
 		fileName:   fileName,
 		timeFormat: timeFormat,
 	}
 }
 
-func (r *TimeRotator) CalculateRotationKey() (key string) {
+func (r *timeRotator) CalculateRotationKey() (key string) {
 	return filepath.Join(r.basePath, fmt.Sprintf(r.fileName, time.Now().Format(r.timeFormat)))
 }
 
-type FileRotator struct {
+type fileRotator struct {
 	basePath  string
 	fileName  string
 	limitLine int64
@@ -37,7 +51,7 @@ func NewFileRotator(basePath, fileName string, limitLine int64) LogRotator {
 		limitLine = 1000
 	}
 
-	return &FileRotator{
+	return &fileRotator{
 		basePath:  basePath,
 		fileName:  fileName,
 		limitLine: limitLine,
@@ -45,6 +59,6 @@ func NewFileRotator(basePath, fileName string, limitLine int64) LogRotator {
 	}
 }
 
-func (r *FileRotator) CalculateRotationKey() (key string) {
+func (r *fileRotator) CalculateRotationKey() (key string) {
 	return filepath.Join(r.basePath, fmt.Sprintf(r.fileName, r.counter.Add(1)%r.limitLine))
 }
--- logger/writer.go
+++ logger/writer.go
@@ -1,158 +1,85 @@
 package logger
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
 	"os"
-	"path/filepath"
-	"sync/atomic"
-	"time"
-
-	"github.com/alioth-center/infrastructure/exit"
-	"github.com/alioth-center/infrastructure/utils/concurrency"
+	"sync"
 )
 
-type Writer interface {
-	Write(data []byte)
-	Close()
+type fileWriter struct {
+	rotator     LogRotator
+	keyCache    string
+	writerCache *os.File
+	cacheMutex  sync.RWMutex
+	logBuffer   chan *bytes.Buffer
+	closer      chan struct{}
 }
 
-var fileWriters = concurrency.NewMap[string, Writer]()
-
-type fileLogWriter struct {
-	f      *os.File
-	buffer chan []byte
-	closed atomic.Bool
+func (fw *fileWriter) WriteRaw(log *bytes.Buffer) {
+	fw.logBuffer <- log
 }
 
-func (fw *fileLogWriter) Write(data []byte) {
-	if !fw.closed.Load() {
-		fw.buffer <- data
-	}
-}
-
-func (fw *fileLogWriter) Close() {
-	if !fw.closed.Load() {
-		fw.closed.Store(true)
-		close(fw.buffer)
-		for data := range fw.buffer {
-			_, _ = fw.f.Write(data)
+func (fw *fileWriter) writeLog(log *bytes.Buffer) (err error) {
+	key := fw.rotator.CalculateRotationKey()
+	fw.cacheMutex.RLock()
+	if key == fw.keyCache {
+		if fw.writerCache != nil {
+			_, err = fw.writerCache.Write(log.Bytes())
+			fw.cacheMutex.RUnlock()
+			return err
 		}
-		_ = fw.f.Close()
+
+		fw.cacheMutex.RUnlock()
+		return errors.New("nil writer cache")
 	}
+	fw.cacheMutex.RUnlock()
+
+	fw.cacheMutex.Lock()
+	if fw.writerCache != nil {
+		_ = fw.writerCache.Close()
+	}
+
+	fw.writerCache, err = os.OpenFile(key, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o755)
+	if err != nil {
+		fw.cacheMutex.Unlock()
+		return errors.New("open log file failed")
+	}
+
+	fw.keyCache = key
+	_, err = fw.writerCache.Write(log.Bytes())
+	if err != nil {
+		fw.cacheMutex.Unlock()
+		return err
+	}
+
+	fw.cacheMutex.Unlock()
+	return nil
 }
 
-func (fw *fileLogWriter) serve() {
-	exit.RegisterExitEvent(func(_ os.Signal) {
-		fw.Close()
-	}, "EXIT_FILE_LOGGER:"+fw.f.Name())
+func (fw *fileWriter) GracefulClose() {
+	fw.closer <- struct{}{}
 
-	for data := range fw.buffer {
-		_, _ = fw.f.Write(data)
-	}
-}
-
-func NewFileWriter(path string) Writer {
-	// exist file writer, return it
-	if w, ok := fileWriters.Get(path); ok {
-		return w
-	}
-
-	f, e := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o666)
-	if e != nil {
-		return nil
-	}
-
-	w := &fileLogWriter{
-		f:      f,
-		buffer: make(chan []byte, 1024),
-		closed: atomic.Bool{},
-	}
-	w.closed.Store(false)
-	go w.serve()
-
-	fileWriters.Set(path, w)
-
-	return w
-}
-
-type rotationFileWriter struct {
-	baseDir  string
-	rotation func(time.Time) string
-	lastFile atomic.Value
-}
-
-func (r *rotationFileWriter) Write(data []byte) {
-	logFile := filepath.Join(r.baseDir, r.rotation(time.Now()))
-	if r.lastFile.Load() != logFile {
-		lastWriter, exist := fileWriters.Get(logFile)
-		if exist && lastWriter != nil {
-			lastWriter.Close()
+	close(fw.logBuffer)
+	for message := range fw.logBuffer {
+		if fw.writerCache != nil {
+			_, _ = fw.writerCache.Write(message.Bytes())
 		}
 	}
 
-	writer := NewFileWriter(logFile)
-	writer.Write(data)
+	_ = fw.writerCache.Close()
 }
 
-func (r *rotationFileWriter) Close() {
-	lastFile := r.lastFile.Load().(string)
-	lastWriter, exist := fileWriters.Get(lastFile)
-	if exist && lastWriter != nil {
-		lastWriter.Close()
-	}
-}
-
-func NewTimeBasedRotationFileWriter(directory string, rotation func(time time.Time) (filename string)) Writer {
-	atValue := atomic.Value{}
-	atValue.Store(filepath.Join(directory, rotation(time.Now())))
-	return &rotationFileWriter{
-		baseDir:  directory,
-		rotation: rotation,
-		lastFile: atValue,
-	}
-}
-
-type consoleWriter struct {
-	console *os.File
-}
-
-func (c consoleWriter) Write(data []byte) {
-	if c.console == os.Stdout || c.console == os.Stderr {
-		_, _ = c.console.Write(data)
-	}
-}
-
-func (c consoleWriter) Close() {}
-
-func NewStdoutConsoleWriter() Writer {
-	return consoleWriter{
-		console: os.Stdout,
-	}
-}
-
-func NewStderrConsoleWriter() Writer {
-	return consoleWriter{
-		console: os.Stderr,
-	}
-}
-
-type multiWriter struct {
-	writers []Writer
-}
-
-func (m multiWriter) Write(data []byte) {
-	for _, writer := range m.writers {
-		writer.Write(data)
-	}
-}
-
-func (m multiWriter) Close() {
-	for _, writer := range m.writers {
-		writer.Close()
-	}
-}
-
-func NewMultiWriter(writers ...Writer) Writer {
-	return multiWriter{
-		writers: writers,
+func (fw *fileWriter) ListenAndServe() {
+	for {
+		select {
+		case log := <-fw.logBuffer:
+			if writeErr := fw.writeLog(log); writeErr != nil {
+				_, _ = fmt.Fprintln(os.Stderr, fmt.Sprintf(`{"log_error": %s, "raw_log": %s}`, writeErr.Error(), log.String()))
+			}
+		case <-fw.closer:
+			return
+		}
 	}
 }
